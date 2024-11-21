9848a37cc6f846068826ea120fd784534a4281db
Sugarragchaa Tumur-Ochir <tumurochirs@berea.edu>
2024-11-21 06:38:36 -0500

    ADDED TOKEN AND AUTH CONTROLLER LOGIC

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -17,20 +17,26 @@ const userSchema = mongoose.Schema(
       required: true,
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
-// Hash password before saving
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Encrypt password using bcrypt
 userSchema.pre("save", async function (next) {
-  // Only hash the password if it has been modified or is new
   if (!this.isModified("password")) {
-    return next();
+    next();
   }
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
-// Create and export the User model
 const User = mongoose.model("User", userSchema);
+
 export default User;
